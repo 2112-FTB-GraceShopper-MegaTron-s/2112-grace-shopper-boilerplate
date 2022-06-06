126108a3c8e8a0a2fa4cd523ad797586a2fed9c6
Eli <elipuga2@gmail.edu>
2022-06-06 09:18:05 -0500

    login compnent code added

--- src/axios-services/index.js
+++ src/axios-services/index.js
@@ -27,6 +27,15 @@ export async function registerUser({username, password, email}){
     console.error(err)
   }
 }
+export async function loginUser({username, password}) {
+  try{
+    const response = await axios.post('/api/users/login',  {username, password})
+    localStorage.setItem('token', response.data.token)
+    return response;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
 export async function getAPIHealth() {
   try {
